af312badc3bfdad1da17d7e47f2fb161a3ef8069
Peter Durham <peterdurham@protonmail.com>
2019-08-04 13:57:00 -0700

    first commit

---
+++ routes/api.js
@@ -0,0 +1,241 @@
+const express = require("express");
+const router = express.Router();
+var request = require("request");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const USER = process.env.RPC_USER;
+const PASS = process.env.RPC_PASSWORD;
+
+const headers = {
+  "content-type": "text/plain;"
+};
+
+router.get("/test", (req, res) => res.json({ msg: "backend works" }));
+
+router.get("/getblockcount", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockcount","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getbestblockhash", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getbestblockhash","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getconnectioncount", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getconnectioncount","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getdifficulty", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getdifficulty","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getblockchaininfo", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockchaininfo","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getmininginfo", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getmininginfo","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getpeerinfo", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getpeerinfo","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getrawmempool", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getrawmempool","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getblock/:hash", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblock","params":["${
+    req.params.hash
+  }"]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getblockhash/:index", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockhash","params":[${
+    req.params.index
+  }]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/getrawtransaction/:id", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getrawtransaction","params":["${
+    req.params.id
+  }"]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+router.get("/decoderawtransaction/:hex", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"decoderawtransaction","params":["${
+    req.params.hex
+  }"]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const rpcMethods = require("./routes/api");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api", rpcMethods);
+
+const port = process.env.PORT || 5000;
+
+server = app.listen(port, () => console.log(`Server running on port ${port}`));
